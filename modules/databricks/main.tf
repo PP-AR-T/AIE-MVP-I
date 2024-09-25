3cd38fe5dda34ccda0729ab6ce940788fc0b0130
PPuente <ppuente@hsdyn.com>
2024-09-25 09:08:40 +0100

    Update main.tf
    Fixing Checkov Comments

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -1,9 +1,11 @@
 resource "azurerm_databricks_workspace" "workspace" {
-  location                    = var.location
-  name                        = var.databricks_workspace_name
-  resource_group_name         = var.resource_group_name
-  sku                         = var.databricks_sku
-  managed_resource_group_name = var.managed_resource_group_name
+  location                      = var.location
+  name                          = var.databricks_workspace_name
+  resource_group_name           = var.resource_group_name
+  sku                           = var.databricks_sku
+  managed_resource_group_name   = var.managed_resource_group_name
+  public_network_access_enabled = false #fixing checkov CKV_AZURE_158
+  #checkov:skip=CKV2_AZURE_48: No need for dbfs encription at this point
   custom_parameters {
     storage_account_name     = var.storage_account_name
     storage_account_sku_name = var.storage_account_sku_name
