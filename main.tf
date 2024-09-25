5813cf93e7a4a411f93d247a880a194fcede0661
PPuente <ppuente@hsdyn.com>
2024-09-25 09:01:43 +0100

    Indentation Fix
    Run Locally terraform fmt to fix

--- main.tf
+++ main.tf
@@ -31,13 +31,13 @@ resource "azurerm_resource_group" "rg-tf-db-ai-demo" {
 module "databricks" {
   source = "./modules/databricks"
 
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  databricks_workspace_name = var.databricks_workspace_name
-  databricks_sku            = var.databricks_sku
+  resource_group_name         = var.resource_group_name
+  location                    = var.location
+  databricks_workspace_name   = var.databricks_workspace_name
+  databricks_sku              = var.databricks_sku
   managed_resource_group_name = var.managed_resource_group_name
-  storage_account_name      = var.storage_account_name
-  storage_account_sku_name  = var.storage_account_sku_name
-  databricks_user_name      = var.databricks_user_name
-  databricks_display_name   = var.databricks_display_name
+  storage_account_name        = var.storage_account_name
+  storage_account_sku_name    = var.storage_account_sku_name
+  databricks_user_name        = var.databricks_user_name
+  databricks_display_name     = var.databricks_display_name
 }
--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_databricks_workspace" "workspace" {
-  location            = var.location
-  name                = var.databricks_workspace_name
-  resource_group_name = var.resource_group_name
-  sku                 = var.databricks_sku
+  location                    = var.location
+  name                        = var.databricks_workspace_name
+  resource_group_name         = var.resource_group_name
+  sku                         = var.databricks_sku
   managed_resource_group_name = var.managed_resource_group_name
   custom_parameters {
-    storage_account_name = var.storage_account_name
+    storage_account_name     = var.storage_account_name
     storage_account_sku_name = var.storage_account_sku_name
   }
 }
